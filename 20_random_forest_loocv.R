b2ac12202b1d3c469a58ab21d497dcc721f5f862
Anthony Doan <mythicalprogrammer@gmail.com>
2019-03-09 17:28:29 -0800

    Fix the logic for loocv so that files per forest are outputted.

--- 20_random_forest_loocv.R
+++ 20_random_forest_loocv.R
@@ -4,8 +4,8 @@ myeloma_data <- read.table("start_data/GDS531_after_anova.csv",
 rand_seed <- 1030
 set.seed(rand_seed)
 nr <- nrow(myeloma_data)
-max_tree <- 1000
-for (j in 1:max_tree) {
+max_tree <- 30
+for (j in 11:max_tree) {
   random_forest_predictions <- list()
   for (i in  1:nr) {
     train <- myeloma_data[-c(i), ]
@@ -15,10 +15,12 @@ for (j in 1:max_tree) {
     random_forest_predictions[[i]] <- pred$predictions
   }
 
+  pred_df <- data.frame(unlist(random_forest_predictions))
+  names(pred_df) <- "predictions"
   # back up the result since this took awhile to run
-  file_path  <- str_c('intermediate_data/LOOCV_random_forest_results_max_tree_',
-                      max_tree, '.csv')
-  write.csv(random_forest_predictions,
-            file = file_path,
-            row.names = FALSE)
+  file_path  <- str_c('intermediate_data/LOOCV_random_forest_results_num_tree_',
+                      j, '.csv')
+  write.csv(pred_df,
+             file = file_path,
+             row.names = FALSE)
 }
